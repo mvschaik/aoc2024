782fad0f600fd284b23890ac8af1d9d6e1c6211d
Maarten van Schaik <maarten@informaarten.nl>
2024-12-07 10:15:53 +0100

    Some stylistic improvements on day 7.

--- src/day07.kt
+++ src/day07.kt
@@ -2,36 +2,26 @@ import java.io.File
 
 infix fun Long.concat(other: Long): Long = (this.toString() + other.toString()).toLong()
 operator fun Pair<Long, Long>.plus(other: Pair<Long, Long>) = (this.first + other.first) to this.second + other.second
-fun Iterable<Pair<Long, Long>>.sum(): Pair<Long, Long> = reduce { p1, p2 -> p1 + p2 }
+fun Iterable<Pair<Long, Long>>.pairwiseSum(): Pair<Long, Long> = reduce { p1, p2 -> p1 + p2 }
 
 fun main(args: Array<String>) {
     val (part1, part2) = File(args[0]).readLines().map { line ->
         val (sumPart, elementsPart) = line.split(": ")
-        val sum = sumPart.toLong()
+        val result = sumPart.toLong()
         val elements = elementsPart.split(" ").map { it.toLong() }
 
-        var part1Values = listOf(elements[0])
-        var part2Values = listOf(elements[0])
-        for (e in elements.drop(1)) {
-            val newPart1Values = mutableListOf<Long>()
-            val newPart2Values = mutableListOf<Long>()
-            for (v in part1Values) {
-                newPart1Values += v + e
-                newPart1Values += v * e
-            }
-            for (v in part2Values) {
-                newPart2Values += v + e
-                newPart2Values += v * e
-                newPart2Values += v concat e
-            }
-            part1Values = newPart1Values
-            part2Values = newPart2Values
+        val p1results = elements.drop(1).fold(listOf(elements[0])) { acc, e ->
+            acc.flatMap { listOf(it + e, it * e) }
         }
+        val p2results = elements.drop(1).fold(listOf(elements[0])) { acc, e ->
+            acc.flatMap { listOf(it + e, it * e, it concat e) }
+        }
+
         Pair(
-            if (sum in part1Values) sum else 0,
-            if (sum in part2Values) sum else 0
+            if (result in p1results) result else 0,
+            if (result in p2results) result else 0
         )
-    }.sum()
+    }.pairwiseSum()
 
     println("Part 1: $part1")
     println("Part 2: $part2")
